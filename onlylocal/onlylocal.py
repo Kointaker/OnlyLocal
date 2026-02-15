2dcda86c637fd513731b563bd383572ab25d123f
Joshua Ferreira <kointaker@Joshuas-MacBook-Air-129.local>
2026-02-15 15:39:23 -0500

    Playing around with encryption

--- onlylocal/onlylocal.py
+++ onlylocal/onlylocal.py
@@ -1,5 +1,23 @@
 # OnlyLocal App
 
+# Imports
+import sqlite3
+from cryptography.fernet import Fernet
+import time
+
+key1 = Fernet.generate_key()
+key1 = Fernet.generate_key()
+key1 = Fernet.generate_key()
+key1 = Fernet.generate_key()
+f = Fernet(key1)
+token = f.encrypt(b"my deep dark secret")
+print(token)
+print(f.decrypt(token))
+print(f)
+
+
+
+
 
 def main():
     pass
